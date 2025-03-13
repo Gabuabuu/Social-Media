1ce9819081ba42224599b4aa93b5671636bbfe5e
Gabryel Neves <gabydavilaneves@gmail.com>
2025-03-12 22:36:07 -0300

    feat: :sparkles: Adding modal with informations about my knowledges in the specifs techs skills below

---
+++ src/js/skillsModal.js
@@ -0,0 +1,42 @@
+function openModal(id) {
+  document.getElementsByClassName("modal")[id].style.display = "flex";
+}
+
+function closeModal(id) {
+  document.getElementsByClassName("modal")[id].style.display = "none";
+}
+
+const style = document.createElement("style");
+style.innerHTML = `
+  .modal {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: rgba(30, 30, 30, 0.84);
+    justify-content: center;
+    align-items: center;
+}
+.modal-content {
+  background: rgba(32, 32, 32, 0.81);
+  padding: 20px;
+  border-radius: 8px;
+  text-align: center;
+  position: relative;
+  border: 2px solid #000000;
+  max-width: 500px;
+  
+}
+
+.close {
+  position: absolute;
+  top: 10px;
+  right: 15px;
+  font-size: 20px;
+  cursor: pointer;
+  color:#ffffff;
+}
+
+`;
+document.head.appendChild(style);
