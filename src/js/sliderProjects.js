ce71547d0fb0c8f56df4fe9dc68cf6a6bcfd6612
Gabryel Neves <gabydavilaneves@gmail.com>
2025-03-24 22:06:47 -0300

    refactor:

--- src/js/sliderProjects.js
+++ src/js/sliderProjects.js
@@ -3,11 +3,11 @@ document.getElementById("radio1").checked = true
 
 setInterval(() => {
   nextImage()
-}, 6000)
+}, 8000)
 
 function nextImage() {
   count++
-  if (count > 4) {
+  if (count > 5) {
     count = 1
   }
   document.getElementById("radio" + count).checked = true
